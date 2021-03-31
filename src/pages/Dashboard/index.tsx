7a869e877e17dffaf667fedcc7594070af83a6c6
Alan Almeida <alanalmeida.emp@gmail.com>
2021-03-31 03:46:42 -0300

    finished Dashboard

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,117 @@
+import { useEffect, useState } from "react";
+import Header from "../../components/Header";
+import ModalAddFood from "../../components/ModalAddFood";
+import ModalEditFood from "../../components/ModalEditFood";
+import Food from '../../components/Food';
+import api from "../../services/api";
+import { FoodsContainer } from "./styles";
+
+interface FoodType {
+  id: number;
+  name: string;
+  description: string;
+  price: string;
+  avaliable: boolean;
+  image: string
+}
+
+export default function Dashboard() {
+
+  const [modalOpen, setModalOpen] = useState(false)
+  const [editModalOpen, setEditModalOpen] = useState(false)
+  const [foods, setFoods] = useState<FoodType[]>([])
+  const [editingFood, setEditingFood] = useState({} as FoodType)
+
+  useEffect(() => {
+    async function loadFoods() {
+      const response = await api.get('/foods')
+
+      setFoods(response.data)
+    }
+
+    loadFoods()
+  }, [])
+
+  function toogleModal() {
+    setModalOpen(!modalOpen)
+  }
+
+  function toogleEditModal() {
+    setEditModalOpen(!editModalOpen)
+  }
+
+  async function handleAddFood(food: FoodType) {
+    try {
+      const response = await api.post('/foods', {
+        ...food,
+        avaliable: true
+      })
+
+      setFoods([...foods, response.data])
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  async function handleUpdateFood(food: FoodType) {
+
+    try {
+      const foodUpdated = await api.put(
+        `/foods/${editingFood.id}`,
+        {...editingFood, ...food}
+      );
+
+      const foodsUpdated = foods.map(f => 
+        f.id !== foodUpdated.data.id ? f : foodUpdated.data  
+      )
+
+      setFoods(foodsUpdated)
+    } catch (error) {
+      console.log(error)
+    }
+
+    
+  }
+
+  async function handleDeleteFood(id: number) {
+    await api.delete(`/foods/${id}`)
+
+    const foodsFiltered = foods.filter(food => food.id !== id)
+
+    setFoods(foodsFiltered)
+  }
+
+  async function handeEditFood(food: FoodType) {
+    setEditingFood(food)
+    setEditModalOpen(true)
+  }
+
+  return (
+    <>
+      <Header openModal={toogleModal} />
+      <ModalAddFood 
+        isOpen={modalOpen}
+        setIsOpen={toogleModal}
+        handleAddFood={handleAddFood}
+      />
+      <ModalEditFood 
+        isOpen={editModalOpen}
+        setIsOpen={toogleEditModal}
+        editingFood={editingFood}
+        handleUpdateFood={handleUpdateFood}
+      />
+      <FoodsContainer data-testid="foods-list">
+        {foods && 
+          foods.map(food => (
+            <Food 
+              key={food.id}
+              food={food}
+              handleDelete={handleDeleteFood}
+              handleEditFood={handeEditFood}
+            />
+          ))
+        }
+      </FoodsContainer>
+    </>
+  )
+}
--- src/pages/Dashboard/index3.jsx
+++ src/pages/Dashboard/index3.jsx
@@ -5,7 +5,7 @@ import api from '../../services/api';
 import Food from '../../components/Food';
 import ModalAddFood from '../../components/ModalAddFood';
 import ModalEditFood from '../../components/ModalEditFood';
-import { FoodsContainer } from './styles';
+import { FoodsContainer } from './styles.ts';
 
 class Dashboard extends Component {
   constructor(props) {
